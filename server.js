3b375a0e1c22efbdc6b217550efa2e3e39e769c9
Aaron <aaronl.brinkley@gmail.com>
2018-02-17 03:26:24 -0500

    Fourth Commit

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 
-require("method-override");
+var methodOverride = require('method-override');
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
 
 
 var exphbs = require("express-handlebars");
@@ -23,4 +27,8 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port)
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+})
