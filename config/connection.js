1c6f411275c894eefa4add6b964d2aaa3d276eb5
Aaron <aaronl.brinkley@gmail.com>
2018-02-17 12:50:50 -0500

    Heroku Troubleshoot #3

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,16 @@ var infoMan = require("../info.js")
 
 var infoWordMan = infoMan.wordThing
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// });
+
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 
 
